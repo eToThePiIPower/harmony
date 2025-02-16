fc39fbd6de075f2d4f04c0624958ff3258d41574
Charles Beynon <eToThePiIPower@gmail.com>
2025-02-16 15:23:02 -0500

    Refactor timeAgo to use npm package

--- assets/js/hooks/Timestamp.js
+++ assets/js/hooks/Timestamp.js
@@ -1,9 +1,13 @@
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en)
+const timeAgo = new TimeAgo('en-US')
+
 const Timestamp = {
   mounted() {
     this.updateTime = () => {
-      const now = new Date();
       const timestamp = new Date(this.el.dataset.timestamp);
-      this.el.textContent = timeAgo(now, timestamp);
+      this.el.textContent = timeAgo.format(timestamp);
     };
     this.intervalId = setInterval(this.updateTime, 1000);
     this.updateTime() // force update the inital absolute timestamps
@@ -11,56 +15,6 @@ const Timestamp = {
   destroyed() {
     clearInterval(this.intervalId);
   }
-}
-
-timeAgo = (now, then) => {
-  var seconds = Math.floor((now - then) / 1000);
-
-  var interval = seconds;
-  if (interval < 10) {
-    return "now";
-  }
-  if (interval < 60) {
-    return "less than a minute ago";
-  }
-
-  interval = seconds / 60;
-  if (interval < 2) {
-    return "a minute ago";
-  }
-  if (interval < 75) {
-    return Math.floor(interval) + " minutes ago";
-  }
-
-  interval = seconds / 3600;
-  if (interval < 2) {
-    return "an hour ago";
-  }
-  if (interval < 30) {
-    return Math.floor(interval) + " hours ago";
-  }
-
-  interval = seconds / 86400;
-  if (interval < 2) {
-    return "a day ago";
-  }
-  if (interval < 38) {
-    return Math.floor(interval) + " days ago";
-  }
-
-  interval = seconds / 2592000;
-  if (interval < 2) {
-    return "a month ago";
-  }
-  if (interval < 13) {
-    return Math.floor(interval) + " months ago";
-  }
-
-  interval = seconds / 31536000;
-  if (interval < 2) {
-    return "a year ago";
-  }
-  return Math.floor(interval) + " years ago";
 }
 
 export default Timestamp;
